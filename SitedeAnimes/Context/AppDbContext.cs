7c14ed22b86f36ee51c84a29eb24397b14009102
Sanmael <samunickinho@gmail.com>
2022-06-01 16:00:03 -0300

    Add project files.

---
+++ SitedeAnimes/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SitedeAnimes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitedeAnimes.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Anime> Animes { get; set; }
+    }
+}
---
+++ SitedeAnimes/Models/Anime.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SitedeAnimes.Models
+{
+    public class Anime
+    {
+        public int AnimeId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string ImagemUrl { get; set; }
+        public bool IsAnimePreferido { get; set; }
+        public int Nota { get; set; }
+        public string SiteAssistirAnime { get; set; }
+        public int GeneroId { get; set; }
+        public virtual Genero Genero { get; set; }
+
+    }
+}
